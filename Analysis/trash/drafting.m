d3a935b1ffe2f7900264b11346e87d4e5b376a35
raniaezzo <ezzo.rania@gmail.com>
2021-11-02 22:35:00 -0400

    removing old files

--- Analysis/trash/drafting.m
+++
@@ -1,9 +0,0 @@
-Location = {'0';'45';'90';'135';'180';'225';'270';'315'};
-Upwards = [;43;38;40;49];
-
-T = struct2table(summary_radial);
-T.Properties.RowNames = summary_radial.rownames;
-T = removevars(T,{'rownames'});
-%T.loc_0
-
-LL_dir = M_raw_concatenated(M_raw_concatenated(:,9)==225,:);
